62b45e27c883b6743600e9ffff7535cc8f334133
Edwis <mantas014@gmail.com>
2017-11-01 16:10:11 +0000

    Upgraded PacemakerAPI and Main
    Reworked PacemakerAPI into a Map keyed on the email address

---
+++ src/controllers/Main.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import java.io.IOException;
+import java.util.List;
+
+import models.User;
+
+public class Main
+{
+  public static void main(String[] args) throws IOException
+  {    
+    PacemakerAPI pacemakerAPI = new PacemakerAPI();
+
+    pacemakerAPI.createUser("Bart",  "Simpson", "bart@simpson.com",  "secret");
+    pacemakerAPI.createUser("Homer", "Simpson", "homer@simpson.com", "secret");
+    pacemakerAPI.createUser("Lisa",  "Simpson", "lisa@simpson.com",  "secret");
+
+    List<User> users = (List<User>) pacemakerAPI.getUsers();
+    System.out.println(users);
+  }
+}
